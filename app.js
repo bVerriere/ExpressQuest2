3c9762d4088fb89d10723f8a3280007c29b19647
Brice <brice.verriere@epitech.eu>
2023-06-14 10:38:54 +0200

    exo 8

--- app.js
+++ app.js
@@ -10,29 +10,6 @@ const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
-const { validateMovie, validateUser } = require("./validators.js");
-
-app.use(express.json());
-app.get("/", welcome);
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.get("/api/users", user.getUsers);
-app.get("/api/users/:id", user.getUsersById);
-
-app.post("/api/movies", validateMovie, movieHandlers.postMovie);
-// app.post("/api/users", validateUser, user.postUser);
-
-app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
-// app.put("/api/users/:id", validateUser, user.updateUser);
-
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-app.delete("/api/users/:id", user.deleteUser);
-
-const { hashPassword } = require("./auth.js");
-
-app.post("/api/users", hashPassword, user.postUser);
-app.put("/api/users/:id", hashPassword, user.updateUser);
-
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
@@ -41,3 +18,28 @@ app.listen(port, (err) => {
   }
 });
 
+const { validateMovie, validateUser } = require("./validators.js");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
+
+//public routes :
+app.use(express.json());
+
+app.get("/", welcome);
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", user.getUsers);
+app.get("/api/users/:id", user.getUsersById);
+
+app.post("/api/users", hashPassword, user.postUser);
+app.post("/api/login", user.getUserByEmailWithPasswordAndPassToNext, verifyPassword);
+
+//protected routes :
+app.use(verifyToken);//Authentication wall : token verification for each of those routes :
+
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.put("/api/users/:id", hashPassword, user.updateUser);
+
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.delete("/api/users/:id", user.deleteUser);
--- user.js
+++ user.js
@@ -109,4 +109,23 @@ const deleteUser = (req, res) => {
     });
 };
 
-module.exports = { getUsers, getUsersById, postUser, updateUser, validateUser, deleteUser };
+const getUserByEmailWithPasswordAndPassToNext = (req, res, next) => {
+  const {email} = req.body;
+  const { password } = req.body;
+  database
+  .query("select * from users where email = ?", [email])
+  .then(([users]) => {
+    if (users[0] != null){
+      req.user = users[0];
+      next();
+    } else {
+      res.sendStatus(401).send("Not Found");
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error retrieving data from database");
+  });
+}
+
+module.exports = { getUsers, getUsersById, postUser, updateUser, validateUser, deleteUser, getUserByEmailWithPasswordAndPassToNext };
